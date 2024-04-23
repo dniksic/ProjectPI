13e9602b45466a9b69e3bcb850a38e33d67eb887
dniksic <dniksic@veleri.hr>
2024-04-23 11:37:21 +0200

    prepoznavanje pravog lica odrađeno, potrebno doradit položaj pravokutnika oko lica s točnim kordinatama bbox-a, usporedba enkodiranih lica sa slica s kamere, uspjeÅšno izvedena

--- Main.py
+++ Main.py
@@ -1,5 +1,10 @@
+import pickle
+import numpy
 import cv2
 import os
+import cvzone
+
+import face_recognition
 
 cap = cv2.VideoCapture(1)
 cap.set(3, 640)
@@ -17,12 +22,43 @@ for path in modoviPutanjaLista:
 
 #print(len(slikeModovaLista)) test
 
-
+#učitavanje enkodiranog filea
+print("Učitavanje enkodirane datoteke započeto...")
+file = open('EncodeFile.p','rb')
+encodeListWithIds = pickle.load(file)
+file.close()
+encodeListKnown, imgIds = encodeListWithIds
+#print(imgIds)
+print("Enkodirana datoteka uspješno učitana...")
 
 while True:
     success, img = cap.read()
+
+    imgS=cv2.resize(img, (0,0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    faceCurFrame=face_recognition.face_locations(imgS)
+    encodeCurFrame=face_recognition.face_encodings(imgS, faceCurFrame)
+
     slikaPozadine[150:150 + 480, 44:44 + 640] = img
     slikaPozadine[50:50 + 600, 790:790 + 400] = slikeModovaLista[0]
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        mathces=face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        #print("matches", mathces)
+        #print("faceDis", faceDis)
+
+        mathcIndex=numpy.argmin(faceDis)
+        #print("Match Index", mathcIndex)
+        if mathces[mathcIndex]:
+            #print("Poznato lice detektirano")
+            #print(imgIds[mathcIndex])
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+       #     bbox = 790+x1, 50+y1, x2-x1, y2-y1
+            cvzone.cornerRect(slikaPozadine, bbox, rt=0)
+
 
     #cv2.imshow("Webkamera", img)
     cv2.imshow("Sistem evidencije", slikaPozadine)
